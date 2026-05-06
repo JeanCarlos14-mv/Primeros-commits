6133d0d4d60ebf8947566c86792ebe62b97d3dd3
JeanCarlos14-mv <jeancarlosdp14@gmail.com>
2026-05-05 23:32:06 -0400

    Calculo de promedio y validacion de nota

--- Tercer_Commit.py
+++ Tercer_Commit.py
@@ -14,3 +14,17 @@ p4 = int(input("periodo 4: "))
 nota_final = (p1 + p2 + p3 + p4) / 4
 
 print(f"El estudiante {nombre_estudiante} tiene un promedio de {round(nota_final)}")
+
+# Validar promedio
+
+if nota_final >= 91:
+    print("Nota exelente")
+
+elif nota_final >= 81:
+    print("Buena nota")
+
+elif nota_final >= 70:
+    print("Debe mejorar")
+
+else:
+    print("EL estudiante debe de ir a clase de verano")
